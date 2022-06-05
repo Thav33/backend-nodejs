3c0001a81dd2b7dc2b51fdcaad6f67be807a5502
victor <iam.vee01@gmail.com>
2022-06-05 13:22:02 +0100

    Send the html file to the browser when user request for it

--- presidential-aspirants-api/server.js
+++ presidential-aspirants-api/server.js
@@ -3,3 +3,14 @@ const app = express();
 const PORT = 5000;
 
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`PRESIDENTIAL ASPIRANTS API SERVER IS RUNNING ON PORT ${PORT}.`)
+})
